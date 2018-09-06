6f3f0f1a24900bfad67065a87a13b3a0536834de
Jasper Haggenburg <jhaggenburg@me.com>
2018-09-06 14:51:15 +0200

    Expose typescript definitions

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+type Props = Object;
+
+interface ObjectWithPromises {
+  [key: string]: Promise<any>
+}
+
+interface Config {
+  shouldPromiseRefresh?: (props: Props) => boolean,
+  preventLogging?: boolean,
+}
+
+export default function promiseContainer(
+  getPromises: (props: Props) => ObjectWithPromises,
+  config?: Config
+): (
+  FulfilledComponent?: React.ComponentType<any>,
+  PendingComponent?: React.ComponentType<any>,
+  RejectedComponent?: React.ComponentType<any>
+) => React.ComponentType<any>
